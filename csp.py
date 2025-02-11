6f1b004834d77c8e035ca6895b34fc7dd61aca73
Gavin Yan <gavinyan10@gmail.com>
2025-02-11 16:32:24 -0500

    added pipetype and variable class

--- csp.py
+++ csp.py
@@ -1,13 +1,42 @@
+from typing import Optional
+
+
+class PipeType:
+    """
+    A pipe type :D
+    Up, right, down, left
+    """
+    def __init__(self, arr: list):
+        if len(arr) == 4:
+            self.arr = arr
+        else:
+            raise Exception("Cannot make pipe type with array of length != 4")
+
 class Variable:
     '''
-    Variable for a CSP.
+    Variable for pipe (each location)
     '''
-    def __init__(self, name, domain):
+    name: tuple
+    domain: list[PipeType]
+
+    def __init__(self, name: tuple, domain: list[PipeType], assignment: Optional[PipeType]):
         self.name = name
-        self.domain = domain.copy()
+        self.domain = domain
+        self.active_domain = domain
+        self.assignment = assignment
 
     def get_domain(self):
-        return self.domain.copy()
+        return list(self.domain)
+    
+    def get_assignment(self):
+        return PipeType(list(self.assignment.arr))
+    
+    def prune(self, to_remove: list[PipeType]):
+        for pipe in to_remove:
+            self.active_domain.remove(pipe)
+        
+    def assign(self, assignment: PipeType):
+        self.assignment = assignment
     
 class Constraint:
     '''
