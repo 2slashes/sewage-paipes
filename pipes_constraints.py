b261099c39ee62c653b7810cd20de96cb1d5ea30
2slashes <embyrne02@gmail.com>
2025-02-11 20:11:38 -0500

    implement not_blocked constraint function

--- pipes_constraints.py
+++ pipes_constraints.py
@@ -2,7 +2,7 @@ from csp import *
 
 def has_connection(vars: list[Variable]) -> bool:
     '''
-    main: the pipe that is being checked to see if there is a connection
+    checks if a pipe has at least one connection with another pipe
     vars[0]: pipe above
     vars[1]: pipe to the left
     vars[2]: pipe below
@@ -21,3 +21,25 @@ def has_connection(vars: list[Variable]) -> bool:
                 if adj_type[(i + 2) % 4]:
                     return True
     return False
+
+def not_blocked(vars: list[Variable]) -> bool:
+    '''
+    ensures that the main pipe is not blocked by another pipe
+    vars[0]: pipe above
+    vars[1]: pipe to the left
+    vars[2]: pipe below
+    vars[3]: pipe to the right
+    vars[4]: main pipe, the one that is being checked
+    '''
+    # get the assignment of the main pipe
+    type = vars[4].get_assignment()
+    for i in range(len(type)):
+        # iterate through the surrounding pipes
+        if type[i]:
+            if vars[i] and vars[i].assignment is not None:
+                # get the type of the corresponding pipe
+                adj_type = vars[i].get_assignment()
+                # check if the adjacent pipe is blocking the current pipe
+                if not adj_type[(i + 2) % 4]:
+                    return False
+    return True
