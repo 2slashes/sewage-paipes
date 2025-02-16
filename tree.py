a10e6ba35e08a8602f3f2e5297aae16fa06277b5
Gavin Yan <gavinyan10@gmail.com>
2025-02-16 01:31:14 -0500

    make tree prune only domains that would cause a loop

--- tree.py
+++ tree.py
@@ -41,9 +41,9 @@ def get_duplicated_touched(
     curr: int,
     assignment: PartialAssignment,
     visited: set[int],
-    touched: set[int],
+    touched: dict[int, int],
     prev: Optional[int] = None,
-) -> Optional[int]:
+) -> Optional[tuple[int, int, int]]:
     """
     Creates a tree from assignment, checks if any squares are touched twice.
     A square is "touched" if a pipe's opening is pointing towards it.
@@ -70,9 +70,9 @@ def get_duplicated_touched(
                 )
 
             if adj_i != prev and adj_i in touched:
-                return adj_i
+                return (adj_i, curr, touched[adj_i])
 
-            touched.add(adj_i)
+            touched[adj_i] = curr
 
     top_pipe, right_pipe, bottom_pipe, left_pipe = [
         assignment[i] if i != -1 else None for i in adj_indexes
@@ -97,26 +97,46 @@ def get_duplicated_touched(
 
 def pruner(variables: list[Variable]) -> dict[Variable, list[PipeType]]:
     assignment: PartialAssignment = [var.get_assignment() for var in variables]
+    n = int(sqrt(len(assignment)))
 
     visited: set[int] = set()
     for assignment_index, assignment_value in enumerate(assignment):
         if assignment_value != None and not assignment_index in visited:
-            duplicate_touch_index = get_duplicated_touched(
+            duplicate_touch = get_duplicated_touched(
                 curr=assignment_index,
                 assignment=assignment,
                 visited=visited,
-                touched=set(),
+                touched={},
             )
 
-            if duplicate_touch_index:
+            if duplicate_touch:
                 variable_to_prune = next(
-                    (var for var in variables if var.location == duplicate_touch_index),
+                    (var for var in variables if var.location == duplicate_touch[0]),
                 )
 
-                pruned_values = {
-                    variable_to_prune: variable_to_prune.active_domain.copy()
-                }
-                variable_to_prune.active_domain.clear()
+                top, right, bottom, left = find_adj(duplicate_touch[0], n)
+                directions = [top, right, bottom, left]
+                touches = [duplicate_touch[1], duplicate_touch[2]]
+                touched_directions: list[int] = []
 
-                return pruned_values
+                for i, direction_value in enumerate(directions):
+                    for touch_value in touches:
+                        if direction_value == touch_value:
+                            touched_directions.append(i)
+
+                pruned_values: list[PipeType] = []
+                for active_domain in variable_to_prune.active_domain:
+                    if (
+                        active_domain[touched_directions[0]]
+                        and active_domain[touched_directions[1]]
+                    ):
+                        pruned_values.append(active_domain)
+
+                pruned_dict: dict[Variable, list[PipeType]] = {
+                    variable_to_prune: pruned_values
+                }
+
+                variable_to_prune.prune(pruned_values)
+
+                return pruned_dict
     return {}
