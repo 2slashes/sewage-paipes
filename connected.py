9d57288d994c9f2d4addfc111c8f4c082444aba5
Gavin Yan <gavinyan10@gmail.com>
2025-02-16 00:01:09 -0500

    added connected pruner
    no graph of graphs

--- connected.py
+++ connected.py
@@ -3,9 +3,8 @@ from csp import Variable
 from pipes_utils import *
 from math import sqrt
 
-def validator(
-    pipes: list[PipeType]
-) -> bool:
+
+def validator(pipes: list[PipeType]) -> bool:
     """
     Ensures that everything is connected
     """
@@ -27,7 +26,9 @@ def validator(
         left_val: Optional[PipeType] = None
         if adj_vals[3] != -1:
             left_val = pipes[adj_vals[3]]
-        connections: tuple[bool, bool, bool, bool] = check_connections(cur, (top_val, right_val, bottom_val, left_val))
+        connections: tuple[bool, bool, bool, bool] = check_connections(
+            cur, (top_val, right_val, bottom_val, left_val)
+        )
         for i in range(4):
             if connections[i] and adj_vals[i] not in visited:
                 stack.append((pipes[adj_vals[i]], adj_vals[i]))
@@ -35,7 +36,41 @@ def validator(
         return True
     return False
 
-def pruner(
-    pipes: list[Variable]
-) -> dict[Variable, list[PipeType]]:
-    
+
+def pruner(variables: list[Variable]) -> dict[Variable, list[PipeType]]:
+    pseudo_assignment = pseudo_assign(variables)
+    can_be_connected = validator(pseudo_assignment)
+    pruned: dict[Variable, list[PipeType]] = {}
+
+    if not can_be_connected:
+        for var in variables:
+            if var.get_assignment() is None:
+                pruned = {var: var.get_active_domain()}
+                var.prune(var.get_active_domain())
+                break
+    return pruned
+
+
+def pseudo_assign(variables: list[Variable]) -> list[PipeType]:
+    pseudo_assignment: list[PipeType] = []
+    for var in variables:
+        assignment = var.get_assignment()
+        if assignment is not None:
+            pseudo_assignment.append(assignment)
+        else:
+            pseudo_pipe: list[bool] = [False, False, False, False]
+            for active_domain in var.get_active_domain():
+                all_true = False
+                for direction in range(4):
+                    if active_domain[direction]:
+                        pseudo_pipe[direction] = True
+                        if sum(pseudo_pipe) == 4:
+                            all_true = True
+                            break
+                if all_true:
+                    break
+            pseudo_assignment.append(
+                (pseudo_pipe[0], pseudo_pipe[1], pseudo_pipe[2], pseudo_pipe[3])
+            )
+
+    return pseudo_assignment
