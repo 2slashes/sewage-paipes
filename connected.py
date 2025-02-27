8d8d9d233a9937036233c705a02c6633655d9c67
Gavin Yan <gavinyan10@gmail.com>
2025-02-27 13:55:32 -0500

    add edge-case for connected pruner when an assignment leads to its own isolation

--- connected.py
+++ connected.py
@@ -1,6 +1,6 @@
 from typing import Optional
 from pipe_typings import PipeType
-from csp import Assignment, Variable, print1DGrid
+from csp import Assignment, Variable, print1DGrid, PIPE_CHAR
 from math import sqrt
 from pipes_utils import find_adj, check_connections
 
@@ -93,6 +93,16 @@ def pruner(variables: list[Variable]) -> dict[Variable, list[PipeType]]:
     disc: dict[int, int] = {}
     low: dict[int, int] = {}
     articulation_points: set[int] = set()
+    result: dict[Variable, list[PipeType]] = {}
+
+    # print1DGrid(pseudo_assignment)  # type: ignore
+
+    isolation_pruned = prune_isolating_assignments(variables)
+    for var in isolation_pruned:
+        if var not in result:
+            result[var] = isolation_pruned[var]
+        else:
+            result[var] += isolation_pruned[var]
 
     find_articulation_points(
         assignment=pseudo_assignment,
@@ -102,7 +112,6 @@ def pruner(variables: list[Variable]) -> dict[Variable, list[PipeType]]:
         low=low,
         articulation_points=articulation_points,
     )
-    result: dict[Variable, list[PipeType]] = {}
     for point in articulation_points:
         var_to_prune = variables[point]
         if var_to_prune.get_assignment() == None:
@@ -115,6 +124,59 @@ def pruner(variables: list[Variable]) -> dict[Variable, list[PipeType]]:
                     else:
                         result[var_to_prune].append(potential_assignment)
                     var_to_prune.prune([potential_assignment])
+
+    # if len(result) != 0:
+    #     for var in result:
+    #         print(f"Pruned {var.location}: {[PIPE_CHAR[pipe] for pipe in result[var]]}")
+    #     a = 2
+
+    return result
+
+
+def prune_isolating_assignments(
+    variables: list[Variable],
+) -> dict[Variable, list[PipeType]]:
+    """
+    Prune assignments that result in that pipe that's does not connect to anything
+    """
+    pseudo_assignment = optimistic_psuedo_assign(variables)
+    unassigned_indicies = [
+        i
+        for i in range(len(pseudo_assignment))
+        if variables[i].get_assignment() == None
+    ]
+    result: dict[Variable, list[PipeType]] = {}
+
+    for i in unassigned_indicies:
+        adj_indices = find_adj(i, int(sqrt(len(pseudo_assignment))))
+        top_val: Optional[PipeType] = None
+        if adj_indices[0] != -1:
+            top_val = pseudo_assignment[adj_indices[0]]
+        right_val: Optional[PipeType] = None
+        if adj_indices[1] != -1:
+            right_val = pseudo_assignment[adj_indices[1]]
+        bottom_val: Optional[PipeType] = None
+        if adj_indices[2] != -1:
+            bottom_val = pseudo_assignment[adj_indices[2]]
+        left_val: Optional[PipeType] = None
+        if adj_indices[3] != -1:
+            left_val = pseudo_assignment[adj_indices[3]]
+        connections: tuple[bool, bool, bool, bool] = check_connections(
+            pseudo_assignment[i], (top_val, right_val, bottom_val, left_val)
+        )
+        openings: list[int] = []
+        for direction in range(4):
+            if connections[direction]:
+                openings.append(direction)
+        for potential_assignment in variables[i].get_active_domain():
+            all_closed = all(not potential_assignment[opening] for opening in openings)
+            if all_closed:
+                if variables[i] not in result:
+                    result[variables[i]] = [potential_assignment]
+                else:
+                    result[variables[i]].append(potential_assignment)
+                variables[i].prune([potential_assignment])
+
     return result
 
 
