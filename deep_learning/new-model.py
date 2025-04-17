02ff8702464a15bba2aea7ba6c2d6735bb5ee922
Gavin Yan <gavinyan10@gmail.com>
2025-04-17 06:19:12 -0400

    add gold standard to the crazy new-model.py file

--- deep_learning/new-model.py
+++ deep_learning/new-model.py
@@ -138,3 +138,159 @@ model_file_name = "model.pth"
 curr_dir = os.getcwd()
 model_file_path = os.path.join(curr_dir, model_file_name)
 torch.save(model.state_dict(), model_file_path)
+
+
+def pick_move(state, visited):
+    # convert the state to integers
+    state_int_list = [int(x) for x in state]
+    # convert the state to a tensor
+    state_tensor = torch.tensor(state_int_list).to(device).float()
+    # get the predicted move from the neural network
+    prob = model(state_tensor)
+    results = torch.topk(prob, 16).indices
+
+    # Initialize the visited state if we haven't seen it before
+    if state not in visited:
+        visited[state] = set()
+
+    # Try each of the top moves
+    result = None
+    for r in results:
+        move = int(r)
+        # Skip if we've already tried this move for this state
+        if not move in visited[state]:
+            result = move
+            break
+
+    if result is None:
+        raise Exception("No valid moves available")
+    return result
+
+
+def pipe_rotate_binary(board: str, pipe: int):
+    """
+    Takes a binary representation of a board of pipes as a string, and a pipe to rotate. Outputs a binary representation of the board after rotating the pipe.
+
+    :params pipe: The pipe to rotate
+    :params board: Binary representation of the board as a string
+
+    """
+    # each pipe has 4 values associated to it, so pipe n starts at index 4 * n
+    start_index = 4 * pipe
+    up = board[start_index]
+    right = board[start_index + 1]
+    down = board[start_index + 2]
+    left = board[start_index + 3]
+
+    # rotate clockwise
+    new_board = (
+        board[:start_index] + left + up + right + down + board[start_index + 4 :]
+    )
+
+    return new_board
+
+
+def get_bad_pipes(state, goal):
+    bad = []
+    # Compare each 4-bit chunk (representing a pipe)
+    for i in range(0, len(state), 4):
+        if state[i : i + 4] != goal[i : i + 4]:
+            bad.append(i // 4)
+    return bad
+
+
+model.eval()
+
+# Load puzzles from `puzzles.csv`
+initials: list[str] = []
+goals: list[str] = []
+prev_moves: list[int] = []
+puzzle_path = os.path.join(curr_dir, "data/puzzles.csv")
+with open(puzzle_path, newline="") as csvfile:
+    reader = csv.reader(csvfile)
+    # skip the header
+    next(reader)
+    for row in reader:
+        initials.append(row[0])
+        goals.append(row[1])
+        prev_moves.append(int(row[2]))
+
+outlier_threshold = 70
+
+corrects = 0
+bad_pipes_when_incorrect = []
+all_moves = []
+extra_moves = []
+bad_pipes_on_moves = []
+normal_initials = []
+normal_goals = []
+
+outlier_corrects = 0
+outlier_bad_pipes_when_incorrect = []
+outlier_all_moves = []
+outlier_extra_moves = []
+outlier_bad_pipes_on_moves = []
+
+outlier_initials = []
+outlier_goals = []
+
+for i in range(len(initials)):
+    initial = initials[i]
+    goal = goals[i]
+    visited = {}
+
+    state = initial
+    visited[initial] = set()
+    cur_puzzle_moves = 0
+    cur_puzzle_bad_pipes_when_incorrect = []
+    cur_puzzle_corrects = 0
+
+    cur_puzzle_bad_pipes_on_moves = []
+
+    while state != goal:
+        move = pick_move(state, visited)
+        bad_pipes = get_bad_pipes(state, goal)
+        cur_puzzle_bad_pipes_on_moves.append(len(bad_pipes))
+        if move in bad_pipes:
+            cur_puzzle_corrects += 1
+        else:
+            cur_puzzle_bad_pipes_when_incorrect.append(len(bad_pipes))
+        visited[state].add(move)
+        state = pipe_rotate_binary(state, move)
+        cur_puzzle_moves += 1
+
+    if len(all_moves) + len(outlier_all_moves) > 0:
+        percentage_outliers = round(
+            len(outlier_all_moves) / (len(all_moves) + len(outlier_all_moves)) * 100, 2
+        )
+    else:
+        percentage_outliers = 0
+    print(f"Solution {i+1}: {cur_puzzle_moves} moves. {percentage_outliers}% outliers")
+
+    if cur_puzzle_moves - prev_moves[i] < 0:
+        raise Exception(
+            f"Solution {i+1} has {cur_puzzle_moves} moves, but the minimum number of moves is {prev_moves[i]}"
+        )
+
+    if cur_puzzle_moves < outlier_threshold:
+        corrects += cur_puzzle_corrects
+
+        all_moves.append(cur_puzzle_moves)
+        extra_moves.append(cur_puzzle_moves - prev_moves[i])
+
+        bad_pipes_on_moves.extend(cur_puzzle_bad_pipes_on_moves)
+        bad_pipes_when_incorrect.extend(cur_puzzle_bad_pipes_when_incorrect)
+
+        normal_initials.append(initial)
+        normal_goals.append(goal)
+    else:
+        outlier_corrects += cur_puzzle_corrects
+
+        outlier_all_moves.append(cur_puzzle_moves)
+        outlier_extra_moves.append(cur_puzzle_moves - prev_moves[i])
+
+        outlier_bad_pipes_when_incorrect.extend(cur_puzzle_bad_pipes_when_incorrect)
+        outlier_bad_pipes_on_moves.extend(cur_puzzle_bad_pipes_on_moves)
+
+        outlier_initials.append(initial)
+        outlier_goals.append(goal)
